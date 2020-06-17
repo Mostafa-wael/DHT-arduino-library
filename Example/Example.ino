feb0cc997f3a14577ed44a182775ca7ae499e40f
Mostafa-wael <56788883+Mostafa-wael@users.noreply.github.com>
2020-06-18 00:21:27 +0200

    Add files via upload

---
+++ Example/Example.ino
@@ -0,0 +1,16 @@
+#include <DHT.h>
+DHT* Pdht=new DHT(2);
+void setup() 
+{
+  Serial.begin (115200);
+}
+
+void loop()
+{
+  Pdht->Serial_print_data();
+
+}
+void serialEvent()
+{
+  Pdht->Serial_print_data();
+}
